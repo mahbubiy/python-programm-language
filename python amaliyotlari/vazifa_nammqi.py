f746f36bbdba2508be78d9167b8f0b4384ae3969
mahbubiy <113010230+mahbubiy@users.noreply.github.com>
2022-09-07 11:59:26 +0500

    Add files via upload

---
+++ python amaliyotlari/vazifa_nammqi.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 20:53:18 2022
+
+@author: User
+After:Bilolxon Muhammadxonov 
+"""
+o = input("RO'YXATTAN O'TISH UCHUN ENTERNI BOSING\n >> ENTER <<  ")
+name = input("Ismingizni kiriting \n>>>")
+surname = input("Familiyangizni kiriting \n>>>")
+age = int(input("Yoshingizni kiriting \n>>>"))
+institut = input("Talim muassasangizni kirting\n>>>")
+group = input("Gruhizzi kiriting\n>>>")
+print(f"\t\t\tMALUMOT \n Ismi:{name.title()}\
+\n Familiyasi:{surname.title()}\
+\n Tug'ilgan yili:{2022-age} - yilada tug'ilgan\
+\n Talim muassasi:{institut.title()}\
+\n Gruppasi:{group.title()}")
+
+
