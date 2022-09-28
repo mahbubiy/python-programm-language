3799353363bfee4dd6883df1a572bfff11c3c1fa
Bilolxon Muxammadxonov 9-ATT-21 <113010230+mahbubiy@users.noreply.github.com>
2022-09-28 20:07:18 +0500

    Add files via upload

---
+++ 3-amaliy-ish.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 20:04:27 2022
+
+@author: User
+"""
+
+number = int(input("son kiriting\n>>>"))
+if number%2==1 and 99<number<1000:
+    print(f"Bu {number} sonni toq va 3 xonali son ")
+else:
+    print("Siz aytgan toqlik va 3 xonali son ga \
+tekshirish o'zini oqlamadi!!!")
