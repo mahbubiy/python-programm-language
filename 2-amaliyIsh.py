8d183db838958bcc1af5064cebaa4c5142e0f601
Bilolxon Muxammadxonov 9-ATT-21 <113010230+mahbubiy@users.noreply.github.com>
2022-09-28 14:17:55 +0500

    Add files via upload

---
+++ 2-amaliyIsh.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 14:16:01 2022
+
+@author: User
+"""
+
+payment=70000
+moonDay = 22
+oneDayPayment = payment/22
+day = int(input("Necha kun boxchaga kelgansiz\n>>>"))
+price = (22-day)*oneDayPayment//1
+print(f"Siz bir oy davomida {22-day} kun kelibsiz va {price}\
+so'm to'lov qilishingiz kerask ekan ") 
