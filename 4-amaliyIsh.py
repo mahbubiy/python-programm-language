50080e0cd7518ceed40cd2b1a50604017ec7f6a3
Bilolxon Muxammadxonov 9-ATT-21 <113010230+mahbubiy@users.noreply.github.com>
2022-12-01 21:21:52 +0500

    Add files via upload

---
+++ 4-amaliyIsh.py
@@ -0,0 +1,17 @@
+# 4.1-amaliy ish topshiriq variantlari
+# number = int(input("Input number > "))
+# ciel = int(input("Input ciel > "))
+# s=1
+# for n in range(1,ciel+1):
+#    s*=number
+# print(s)
+
+
+
+# 4.1-amaliy ish topshiriq variantlari
+numbers = {1,2,5,8,63,2,0}
+sanoq=0
+for m in numbers:
+    sanoq+=1
+print(sanoq)
+
