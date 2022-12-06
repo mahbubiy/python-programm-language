ac84c5bbb98d9aaeeea97bfc81be2fb8303bbbb5
Bilolxon Muxammadxonov 9-ATT-21 <113010230+mahbubiy@users.noreply.github.com>
2022-12-06 23:47:37 +0500

    Add files via upload

---
+++ 1-tarjira.py
@@ -0,0 +1,5 @@
+# 12 - Savol 
+input("Aylananing diametri d berilgan. Uning uzunligini topish dasturi tuzilsin. =3,14 ga teng deb olinsin.\n\t\t Agar savol chunarli bo'lsa ENTER ni bosing>>>")
+d = int(input("d = "))
+p = 3.14
+print(f"L={2*p*(d/2)}")
